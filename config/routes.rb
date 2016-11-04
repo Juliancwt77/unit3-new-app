8a5715e7a933bdd535949b386164d919fd6efc1b
Julian Chung <juliancwt77@gmail.com>
2016-11-04 12:40:47 +0800

    find by id

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
-get '/help', to: 'application#help'
+resources :users
 
+get '/help', to: 'application#help'
+get '/index', to: 'user#index'
+get '/show', to: 'user#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'application#home'
+root 'application#home'
 
 end
