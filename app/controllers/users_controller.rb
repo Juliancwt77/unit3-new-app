fb144eded4f42f2ae6fd7bce96648c477a507919
Julian Chung <juliancwt77@gmail.com>
2016-11-04 16:31:35 +0800

    form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,50 @@
 class UsersController < ApplicationController
 
-def index
-
+  def index
     @users = User.all
+  end
 
-end
-
-def show
-
+  def show
     @users = User.find(params[:id])
+  end
 
-end
+  def new
+    @user = User.new
+  end
+
+  # def create
+  #
+  #   @new_user = User.new()
+  #   @new_user.name = params[:user][:name]
+  #   @new_user.email = params[:user][:email]
+  #   @new_user.save
+  #
+  #   # redirect_to @new_user
+  #
+  #   redirect_to users_path
+  #
+  # end
+
+  def create
+     # debugger
+     @user = User.new(user_params)
+     @user.save
+    #  redirect_to @user
+     redirect_to users_path
+  end
+
+def update
+
+
+  end
+
+   def delete
+   end
+
+   private
+
+   def user_params
+     params.require(:user).permit(:name, :email)
+   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
 resources :users
 
 get '/help', to: 'application#help'
-get '/index', to: 'user#index'
-get '/show', to: 'user#show'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
