995b2dbb4c9db70a0151d55692004bd4372753b9
Julian Chung <juliancwt77@gmail.com>
2016-11-07 16:21:36 +0800

    heroku update

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ get '/help', to: 'application#help'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-root 'application#home'
+root 'application#home'     
 
 end
